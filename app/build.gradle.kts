35918c4253a5b88d06122a5c338de9098d488aca
zhangkunyuan <zhangkunyuan@cmhi.chinamobile.com>
2025-12-04 14:31:08 +0800

    Auto-inject version name from git tag during CI build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 28
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = System.getenv("VERSION_NAME") ?: "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
